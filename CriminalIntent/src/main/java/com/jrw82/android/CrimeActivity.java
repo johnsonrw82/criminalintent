b8645dfb0df988e934aa8e6971cad27daf47f96f
johnsonrw82 <johnsonrw82@gmail.com>
2015-03-28 12:54:30 -0700

    Create the Crime Fragment and begin transaction.

--- CriminalIntent/src/main/java/com/jrw82/android/CrimeActivity.java
+++ CriminalIntent/src/main/java/com/jrw82/android/CrimeActivity.java
@@ -2,7 +2,9 @@ package com.jrw82.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 
 public class CrimeActivity extends FragmentActivity {
@@ -17,6 +19,16 @@ public class CrimeActivity extends FragmentActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if ( fragment == null ) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
     }
 
 }
